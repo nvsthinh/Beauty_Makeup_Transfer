e2d689c09671de6e53a0ad5d52e9ca74d07a84d3
Truong Tuan Phi <fitruong02.work@gmail.com>
2024-01-25 13:22:39 +0700

    update postgres_connector

--- sourse/postgres_connector.py
+++ sourse/postgres_connector.py
@@ -1,34 +1,82 @@
 # sourse/postgres_connector.py
 import psycopg2
-
-def read_database_config(file_path="database_init.txt"):
+import api
+def read_db_config(file_path="database_init.txt"):
     with open(file_path, "r") as file:
         lines = file.readlines()
         return {line.split('=')[0]: line.split('=')[1].strip() for line in lines}
 
-database_config = read_database_config()
 
-try:
-    conn = psycopg2.connect(
-        host=database_config["hostname"],
-        database=database_config["dbname"],
-        user=database_config["username"],
-        password=database_config["password"]
-    )
-    print(f"Successfully connected to the database with name: {database_config["dbname"]}")
-except Exception as e:
-    print(f"Failed to connect to the database. Error: {e}")
+def connect_db(db_config):
+    try:
+        conn = psycopg2.connect(
+            host=db_config["hostname"],
+            database=db_config["dbname"],
+            user=db_config["username"],
+            password=db_config["password"]
+        )
+        print(f"Successfully connected to the database with name: {db_config["dbname"]}")
+        return conn
+    except Exception as e:
+        print(f"Failed to connect to the database. Error: {e}")
 
-cur = conn.cursor()
-def save_user_record_to_db(user_id: str):
-    user_record = user_records[user_id]
-    cur.execute(
-        "INSERT INTO user_records (user_id, ip_address, org_img, style_id, result) VALUES (%s, %s, %s, %s, %s)",
-        (user_id, user_record["ip_address"], user_record["org_img"], user_record.get("style_id"), user_record.get("result"))
-    )
-    conn.commit()
+def save_record(user_records):
+    try:
+        cur.execute(
+            "INSERT INTO user_records (user_id, ip_address, org_img, style_id, result) VALUES (%s, %s, %s, %s, %s)",
+            (user_id, user_record["ip_address"], user_record["org_img"], user_record.get("style_id"), user_record.get("result"))
+        )
+        print(f"Successfully inserted")
+        conn.commit()
+    except Exception as e:
+        print(f"Failed to insert the record. Error: {e}")
+        conn.rollback()
 
-def get_user_record_from_db(user_id: str):
+def get_user(user_id: str):
     cur.execute("SELECT * FROM user_records WHERE user_id = %s", (user_id,))
     db_record = cur.fetchone()
     return {"ip_address": db_record[2], "org_img": db_record[3], "style_id": db_record[4], "result": db_record[5]}
+def show_all_tables(cursor):
+
+    # Lấy tất cả các bảng trong schema 'public'
+    cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
+
+    # Sử dụng fetchall() để lấy danh sách tên bảng
+    tables = cursor.fetchall()
+
+    #close
+    cursor.close()
+
+    # Trả về số lượng bảng
+    return tables
+def group_style(cursor,groupStyle:str):
+    try:
+        # Thực hiện truy vấn với tham số
+        cursor.execute('SELECT * FROM Styles WHERE GroupType = %s', (groupStyle,))
+
+        # Lấy tất cả kết quả
+        results = cursor.fetchall()
+        for row in results:
+            print(row[2])
+        # Kiểm tra xem có kết quả hay không
+        if not results:
+            return "Không tìm thấy kết quả cho {}".format(groupStyle)
+        else:
+            return results
+    except Exception as e:
+        return f"Something went wrong. Error: {e}"
+    finally:
+        #close cursor
+        cursor.close()
+
+if __name__ == "__main__":
+    db_config = read_db_config()
+    conn = connect_db(db_config)
+    cur1 = conn.cursor() #tạo cursor 1
+    print(show_all_tables(cur1))
+    cur2 = conn.cursor() #tạo cursor 2
+    print(group_style(cur2,"Work Style"))
+
+    # user_records = api.get_user_record()  #return dict
+    # save_record(user_records)
+    conn.close()
